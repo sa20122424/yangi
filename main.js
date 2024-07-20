f33cba632ab7b98b8d2c4ff34f055ca0fabbb0b4
sa20122424 <saloxiddin201224@gmail.com>
2024-07-20 17:17:45 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+const username = prompt("ismingiz")
+const userAge = prompt("porilini kiriting")
+console.log(username);
+console.log(userAge);
+
+alert(`
+sizning ismingiz ${username} 
+sizning parolingiz ${userAge} 
+    `)
+
+  
+alert("ismingiz va parolingizni yana kiriting ")  
+const usename = prompt("ismingiz")
+const useAge = prompt("porilini kiriting")
+  
+console.log(usename);
+console.log(useAge);  
+
+if (username == usename + userAge == useAge 
+   ) {
+    alert("togri mas eshak ")
+  } else {
+    alert("malades :)  ")
+  }
+  
+  
+  
+
